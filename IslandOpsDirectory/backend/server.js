dbed9862b8c566152cef7a451fbb0d163aaa4f56
Your Name <you@example.com>
2025-03-23 22:16:20 -0400

    added comments and added database connection check

--- IslandOpsDirectory/backend/server.js
+++ IslandOpsDirectory/backend/server.js
@@ -5,14 +5,27 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const pool = require('./database');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
 
+connect();
+async function connect() {
+    try{
+        // tests to see if database is connected by trying to make a query
+        await pool.query('SELECT * FROM user');
+        console.log(`Connected`);
+    }
+    catch (e) {
+        console.error(`Connection failed ${e}`);
+    }
+}
+
 app.get('/', (req, res) => {
-    res.send('Welcome to the Backend API');
+    res.send('Welcome to the Backend');
 });
 
 app.listen(PORT, () => {
