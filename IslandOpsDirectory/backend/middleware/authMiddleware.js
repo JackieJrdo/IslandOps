188d68410e602d2b72c4f890d107712dc53866ca
Your Name <you@example.com>
2025-03-23 22:45:29 -0400

    forgot to save a file i changed so committing those changes now

--- IslandOpsDirectory/backend/middleware/authMiddleware.js
+++ IslandOpsDirectory/backend/middleware/authMiddleware.js
@@ -2,18 +2,22 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const authenticateUser = (req, res, next) => {
+    // gets the token from the authorization heading
     const token = req.header("Authorization");
-
+    // if the token isn't provided, then throw an error and deny access
     if(!token) {
         return res.status(401).json({error: "Access denied. No token provided"});
     }
 
     try {
+        // decodes the jwt to find out who the user is
         const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
+        // assigns the decoded user info to the request object
         req.user = decoded;
         next();
     }
     catch(err) {
+        // throws an error if the jwt is unable to be decoded
         res.status(400).json({ error: "Invalid token" });
     }
 };
