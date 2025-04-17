35056d5e15eda6d2b0cd80a197f6f58d1faec7ac
SweatierFoil840 <seth.sainz@ufl.edu>
2025-04-17 14:34:47 -0400

    Addition of about and contact pages, as well as changes to App.jsx file to support the added pages

--- IslandOpsDirectory/client/src/App.jsx
+++ IslandOpsDirectory/client/src/App.jsx
@@ -1,13 +1,18 @@
+
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Contact from './pages/Contact'
+import About from './pages/About'
 import './App.css';
 
 /**
  * main app component
  * handles routing and main application structure
  * login page is the default route for unauthenticated users
+ * dashboard is temporarily set as default route for development
  */
 function App() {
   return (
@@ -15,9 +20,16 @@ function App() {
       <Routes>
         {/* login is the default route */}
         <Route path="/" element={<Login />} />
+        {/* temporarily setting dashboard as default route */}
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
         {/* redirect any unknown routes to login */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* redirect any unknown routes to dashboard */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
